dc74618cbbd6c51a9d1ac92c7412c14e533a7d2c
Beazlerat <mara.cutler@gmail.com>
2020-12-08 14:57:02 -0700

    finished purchases component

--- src/actions/user.js
+++ src/actions/user.js
@@ -8,14 +8,92 @@ export function fetchUserPurchases() {
         payload: [
             {
                 _id: 0,
-                title: 'purchase 1',
-                amount: 8.02 
+                total: 19.40,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
             },
             {
                 _id: 1,
-                title: 'purchase 2',
-                amount: 19.40 
-            }
+                total: 1.40,
+                orderNumber: 'B00488343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'NOT Hudgens',
+                    shippingAddress: 'nope East State Street'
+                }
+            },
+            {
+                _id: 2,
+                total: 14.40,
+                orderNumber: 'ASD56789343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'NIKI Hudgens',
+                    shippingAddress: 'middle of no where'
+                }
+            },
+            {
+                _id: 3,
+                total: 79.00,
+                orderNumber: 'BBBBBB48343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Max Noone',
+                    shippingAddress: 'space space space'
+                }
+            },
+            {
+                _id: 4,
+                total: 9.40,
+                orderNumber: 'ADDDD8343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Yes No',
+                    shippingAddress: 'hot and cold 1234'
+                }
+            },
+            {
+                _id: 5,
+                total: 19.40,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 6,
+                total: 19.40,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'Jordan Hudgens',
+                    shippingAddress: '1234 West State Street'
+                }
+            },
+            {
+                _id: 7,
+                total: 19.89,
+                orderNumber: 'A0048248343',
+                orderDate: new Date(),
+                creditCard: '-0000',
+                user: {
+                    name: 'heyo yakko',
+                    shippingAddress: 'warner lot and tower'
+                }
+            },
         ]
     })
 }
