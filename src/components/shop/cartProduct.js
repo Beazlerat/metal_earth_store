0fa385dd5a81848105fb9791e1e736c2f582f9ee
Beazlerat <mara.cutler@gmail.com>
2020-12-15 08:41:50 -0700

    cart product grid

--- src/components/shop/cartProduct.js
+++ src/components/shop/cartProduct.js
@@ -5,7 +5,8 @@ import GreenPriceTag from '../greenPriceTag';
 class CartProduct extends Component {
     render() {
         const { className, product, quantity } = this.props;
-        const { title, price} = product;        
+        const { title, price} = product; 
+        return (       
             <div className={`${className} cart-product`}>
                 <img className='cart-product__image' src='http://via.placeholder.com/130x130'/>
                 <div className='cart-product__title'>{title}</div>
